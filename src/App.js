480497ecd4b7c7ac7f9f9303edaeb9a41f1b5400
saikiran.bandaru <saikiran.bandaru@MQADS.HYD>
2021-02-06 21:24:35 +0530

    added about section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade'
+import {Parallax}  from 'react-parallax'
+import Container from 'react-bootstrap/Container'
+import MyCarousel from './components/myCarousel/MyCarousel'
+import MyNavbar from './components/mynavbar/MyNavbar'
+import TitleMessage from './components/title-message/TitleMessage'
+import About from './pages/about/about'
+import BgImage from "./assets/img/parallex/background.webp";
+
+
 import './App.css';
-import MyCarousel from './components/myCarousel/MyCarousel';
-import MyNavbar from './components/mynavbar/MyNavbar';
-import TitleMessage from './components/title-message/TitleMessage';
+
+
 
 const App=()=> {
   return (
-    <div className="App">
+    <div className="App" style={{ position: "relative" }}>
      <MyNavbar />
      <MyCarousel />
      <TitleMessage/>
+     <div>
+        <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={BgImage}
+          bgImageAlt=""
+          strength={-200}
+        >
+          <div>
+            <Container className="container-box rounded">
+              <Fade duration={500}>
+                <About />
+              </Fade>
+            </Container>
+          </div>
+        </Parallax>
+      </div>
     </div>
   );
 }
