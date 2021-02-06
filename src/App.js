b6e1f289a8d9f13c0a0921105679ac7eaa2f42cb
saikiran.bandaru <saikiran.bandaru@MQADS.HYD>
2021-02-06 10:07:34 +0530

    initial commit my navbarcomponent

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import MyNavbar from './components/mynavbar/MyNavbar';
+
+const App=()=> {
+  return (
+    <div className="App">
+     <MyNavbar />
+     My Protofolio
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/mynavbar/MyNavbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import logo from '../../assets/icons/logow.webp'
+
+import './mynavbar.style.css'
+
+const MyNavbar = () => {
+    return (
+        <>
+  <Navbar fixed="top" collapseOnSelect expand="md"  variant="dark" className="animate-navbar nav-theme justify-content-between">
+  <Navbar.Brand href="#home">
+      <img className="logo" src={logo} alt="MyProtofolio Logo"/>
+  </Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="ml-auto">
+      <Nav.Link href="#Home">Home</Nav.Link>
+      <Nav.Link href="#About">About</Nav.Link>
+      <Nav.Link href="#Skills">Skills</Nav.Link>
+      <Nav.Link href="#Experience">Experience</Nav.Link>
+      <Nav.Link href="#Projects">Projects</Nav.Link>
+      <Nav.Link href="#Contact Me">Contact Me</Nav.Link>
+
+    </Nav>
+  </Navbar.Collapse>
+</Navbar> 
+        </>
+    )
+}
+
+export default MyNavbar
